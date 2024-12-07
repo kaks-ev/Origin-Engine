02a501ab7399c574b47a1bebd1a46b29f82b8e98
Evangelion Manuhutu <evangelion11.m@gmail.com>
2024-12-08 02:02:42 +0700

    Added ubuntu version detection

--- gen.sh
+++ gen.sh
@@ -4,6 +4,10 @@
 # to the current user
 sudo chown -R $USER: $HOME
 
+# detect ubuntu version
+VERSION=$(lsb_release -rs)
+echo "Running script for Ubuntu-$VERSION"
+
 # install dependenceis
 sudo apt update
 sudo apt install libwayland-dev libxkbcommon-dev xorg-dev zlib1g-dev libfmt-dev zenity gdb libgmock-dev ninja-build -y
@@ -16,9 +20,21 @@ sudo apt update
 sudo apt install build-essential mono-complete mono-devel mono-dbg libicu-dev -y
 sudo snap install dotnet-sdk --classic
 
-# Vulkan installation
-wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
-sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-jammy.list http://packages.lunarg.com/vulkan/lunarg-vulkan-jammy.list
+
+if [[ "$VERSION" == "24.04" ]]; then
+    echo "Installing Vulkan SDK for Ubuntu 24.04 (Noble)"
+    wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
+    sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-noble.list http://packages.lunarg.com/vulkan/lunarg-vulkan-noble.list
+elif [[ "$VERSION" == "22.04" ]]; then
+    echo "Installing Vulkan SDK for Ubuntu 22.04 (Jammy)"
+    wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
+    sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-jammy.list http://packages.lunarg.com/vulkan/lunarg-vulkan-jammy.list
+else
+    echo "Unsupported Ubuntu version: $VERSION"
+    echo "This script only supports Ubuntu 22.04 and 24.04."
+    exit 1
+fi
+
 sudo apt update
 sudo apt install vulkan-sdk -y
 sudo apt install libvulkan1 mesa-vulkan-drivers vulkan-utils -y
